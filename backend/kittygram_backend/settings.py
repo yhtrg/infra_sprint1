bd2570181c36cf4fc9d023caff073d53bbb8af53
yhtrg <Ever-Kartashyan1234@yandex.ru>
2023-07-08 09:34:00 +0000

    hw11

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,14 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['51.250.22.214', '127.0.0.1', 'localhost', 'kittyflue.hopto.org']
+ALLOWED_HOSTS = [os.getenv('IP'), '127.0.0.1', 'localhost', os.getenv('PRIVATE_DNC')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
